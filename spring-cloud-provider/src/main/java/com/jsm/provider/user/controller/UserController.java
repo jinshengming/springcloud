1fb07b4ab1b1ed80ea41cdbb25b98a465008608f
jinshengming <qq262152>
2021-05-08 18:46:04 +0800

    添加依赖

--- spring-cloud-provider/src/main/java/com/jsm/provider/user/controller/UserController.java
+++ spring-cloud-provider/src/main/java/com/jsm/provider/user/controller/UserController.java
@@ -6,14 +6,12 @@ import com.jsm.provider.user.dto.UserDTO;
 import com.jsm.provider.user.service.UserService;
 import com.jsm.provider.user.struct.UserStruct;
 import com.jsm.provider.user.vo.UserVO;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 
-@Slf4j
 @RestController
 public class UserController extends BaseController {
 
--- spring-cloud-provider/src/main/java/com/jsm/provider/user/service/impl/UserServiceImpl.java
+++ spring-cloud-provider/src/main/java/com/jsm/provider/user/service/impl/UserServiceImpl.java
@@ -3,11 +3,9 @@ package com.jsm.provider.user.service.impl;
 import com.jsm.common.util.service.impl.BaseServiceImpl;
 import com.jsm.provider.user.dto.UserDTO;
 import com.jsm.provider.user.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
-@Slf4j
 @Service
 public class UserServiceImpl extends BaseServiceImpl implements UserService {
 
