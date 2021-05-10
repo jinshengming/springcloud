dddccad108536324d065cb8dd0af430cef41c8e7
jinshengming <qq262152>
2021-05-10 09:50:55 +0800

    添加依赖

--- spring-cloud-common/src/main/java/com/jsm/common/util/controller/BaseController.java
+++ spring-cloud-common/src/main/java/com/jsm/common/util/controller/BaseController.java
@@ -1,10 +1,6 @@
 package com.jsm.common.util.controller;
 
-import com.github.pagehelper.Page;
-import com.jsm.common.util.dto.ResultDTO;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.util.List;
 
 /**
  * @author 金生明
@@ -12,15 +8,15 @@ import java.util.List;
 @CrossOrigin(exposedHeaders = {"Authorization", "authorization"})
 public class BaseController {
 
-    public <T> ResultDTO<List<T>> handlePage(Page<T> page) {
-        ResultDTO<List<T>> result = ResultDTO.ok();
-        List<T> data = page.getResult();
-        result.setPageCount(page.getPages());
-        result.setTotal(page.getTotal());
-        result.setPageNum(page.getPageNum());
-        result.setPageSize(page.getPageSize());
-        result.setData(data);
-        return result;
-    }
+//    public <T> ResultDTO<List<T>> handlePage(Page<T> page) {
+//        ResultDTO<List<T>> result = ResultDTO.ok();
+//        List<T> data = page.getResult();
+//        result.setPageCount(page.getPages());
+//        result.setTotal(page.getTotal());
+//        result.setPageNum(page.getPageNum());
+//        result.setPageSize(page.getPageSize());
+//        result.setData(data);
+//        return result;
+//    }
 
 }
--- spring-cloud-common/src/main/java/com/jsm/common/util/service/BaseService.java
+++ spring-cloud-common/src/main/java/com/jsm/common/util/service/BaseService.java
@@ -1,16 +1,12 @@
 package com.jsm.common.util.service;
 
-import com.github.pagehelper.Page;
-
-import java.util.Collection;
-
 public interface BaseService {
 
 
 
 
 
-    <E> Page<E> handlePage(Page<Object> page, Collection<E> colls);
+    //<E> Page<E> handlePage(Page<Object> page, Collection<E> colls);
 
 
 
